3102ca29498ece53e9ea34b952bc4dc3b188b28c
Miklos Komcsak <komcsakmiklos@gmail.com>
2021-04-18 19:38:31 +0200

    countries step3

--- Part-2/countries/src/App.js
+++ Part-2/countries/src/App.js
@@ -4,10 +4,12 @@ import axios from "axios";
 function App() {
   const [searchValue, setSearchValue] = useState("");
   const [countries, setCountries] = useState([]);
+  const [weatherDataTemp, setWeatherDataTemp] = useState("");
+  const [weatherDataWeatherIcon, setWeatherDataWeatherIcon] = useState("10d");
+  const [weatherDataWind, setWeatherDataWind] = useState("");
 
   useEffect(() => {
     axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
-      console.log(response.data);
       setCountries(response.data);
     });
   }, []);
@@ -20,22 +22,34 @@ function App() {
     );
 
     if (filteredCountries.length === 1) {
-      let weatherParams = {
-        access_key: "c54c7600a7e6b2a1708c92ab7fe5037b",
-        query: `${filteredCountries[0].capital}`,
-      };
-
-      console.log(weatherParams);
+      let weatherCity = filteredCountries[0].capital;
 
       axios
-        .get("https://api.weatherstack.com/current", { weatherParams })
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${weatherCity}&units=metric&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`
+        )
         .then((response) => {
-          console.log(response.data);
+          setWeatherDataTemp(response.data.main.temp);
+          setWeatherDataWeatherIcon(response.data.weather[0].icon);
+          setWeatherDataWind(response.data.wind.speed);
         });
     }
-    console.log(filteredCountries);
 
     setCountries(filteredCountries);
+  };
+
+  const showCountry = (name) => {
+    let selectedCountry = countries.filter((country) => country.name === name);
+    setCountries(selectedCountry);
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${selectedCountry[0].capital}&units=metric&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`
+      )
+      .then((response) => {
+        setWeatherDataTemp(response.data.main.temp);
+        setWeatherDataWeatherIcon(response.data.weather[0].icon);
+        setWeatherDataWind(response.data.wind.speed);
+      });
   };
 
   return (
@@ -48,7 +62,12 @@ function App() {
       {countries.length < 10 &&
         countries.length > 1 &&
         countries.map((country, i) => {
-          return <div key={i}>{country.name}</div>;
+          return (
+            <div key={i}>
+              <span>{country.name}</span>
+              <button onClick={() => showCountry(country.name)}>show</button>
+            </div>
+          );
         })}
       {countries.length === 1 && (
         <div>
@@ -61,7 +80,17 @@ function App() {
               return <li key={i}>{language.name}</li>;
             })}
           </ul>
-          <img src={`${countries[0].flag}`} alt="flag" width="80px" />
+          <img src={countries[0].flag} alt="flag" width="80px" />
+          <div>
+            <h3>Weather in {countries[0].name}</h3>
+            <p>temperature: {weatherDataTemp} Celcius</p>
+            <img
+              src={`http://openweathermap.org/img/wn/${weatherDataWeatherIcon}@2x.png`}
+              alt="weather-icon"
+              width="80px"
+            />
+            <p>wind: {weatherDataWind} mps</p>
+          </div>
         </div>
       )}
     </div>
