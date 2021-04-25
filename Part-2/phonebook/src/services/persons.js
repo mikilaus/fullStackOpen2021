a0be86783a938d76cb2a3138669c2a995c43140d
Miklos Komcsak <komcsakmiklos@gmail.com>
2021-04-25 18:41:16 +0200

    3.11 phonebook full stack

--- Part-2/phonebook/src/services/persons.js
+++ Part-2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const serverUrl = "http://localhost:3001/persons";
+const serverUrl = "/api/persons";
 
 export const getAll = () => {
   const request = axios.get(serverUrl);
