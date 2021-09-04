6ab4de00c406bdf73bc8e73548d6194dd2ab3e8d
Miklos Komcsak <komcsakmiklos@gmail.com>
2021-09-04 16:49:29 +0200

    Authors view

---
+++ GraphQL-library-frontend/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Authors from "./components/Authors";
+import Books from "./components/Books";
+import NewBook from "./components/NewBook";
+
+import { ALL_PERSONS } from "./queries/queries";
+import { useQuery } from "@apollo/client";
+
+const App = () => {
+  const [page, setPage] = useState("authors");
+
+  const result = useQuery(ALL_PERSONS);
+
+  console.log(result);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage("authors")}>authors</button>
+        <button onClick={() => setPage("books")}>books</button>
+        <button onClick={() => setPage("add")}>add book</button>
+      </div>
+
+      <Authors
+        show={page === "authors"}
+        authors={result.loading ? [] : result.data.allAuthors}
+      />
+
+      <Books show={page === "books"} />
+
+      <NewBook show={page === "add"} />
+    </div>
+  );
+};
+
+export default App;
---
+++ GraphQL-library-frontend/src/components/Authors.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Authors = (props) => {
+  if (!props.show) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {props.authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+Authors.propTypes = {
+  show: PropTypes.bool,
+  authors: PropTypes.array,
+};
+
+export default Authors;
